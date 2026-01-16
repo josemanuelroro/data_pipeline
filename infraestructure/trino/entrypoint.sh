6d8ca58f4ff05a273525d819100cfdc021262f69
jose <josemanuelr90@gmail.com>
2026-01-16 10:51:06 +0100

    add delta format file in spark and trino

--- infraestructure/trino/entrypoint.sh
+++ infraestructure/trino/entrypoint.sh
@@ -4,4 +4,7 @@ sed -e "s|\${MINIO_USER}|$MINIO_USER|g" \
     -e "s|\${MINIO_PASSWORD}|$MINIO_PASSWORD|g" \
     /etc/trino/catalog/minio.properties.template > /etc/trino/catalog/minio.properties
 
+sed -e "s|\${MINIO_USER}|$MINIO_USER|g" \
+    -e "s|\${MINIO_PASSWORD}|$MINIO_PASSWORD|g" \
+    /etc/trino/catalog/delta.properties.template > /etc/trino/catalog/delta.properties
 exec /usr/lib/trino/bin/run-trino
